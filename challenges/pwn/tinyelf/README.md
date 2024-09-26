eaa707f3b4497caa31c427898969d4644b7184f0
13m0n4de <136407746+13m0n4de@users.noreply.github.com>
2024-09-27 03:56:03 +0800

    feat(pwn/tinyelf): 添加题目「我再也不要出有附件的题目了」 (#22)
    * ci(pwn/tinyelf): 添加工作流文件
    
    * docs(pwn/tinyelf): 添加 README
    
    * feat(pwn/tinyelf): 添加源码和镜像构建文件
    
    * chore(pwn/tinyelf): update attachments [skip ci]
    
    * feat(pwn/tinyelf): 添加题解脚本
    
    * refactor(pwn/tinyelf): 更改读入大小，更新附件 Base64
    
    * chore(pwn/tinyelf): update attachments [skip ci]

---
+++ challenges/pwn/tinyelf/README.md
@@ -0,0 +1,24 @@
+# 我再也不要出有附件的题目了
+
+- 作者：13m0n4de
+- 参考：-
+- 难度：-
+- 分类：Pwn
+- 镜像：-
+- 端口：-
+
+## 题目描述
+
+想象一下，出题人如西西弗一般，日复一日地推动着题目附件这块沉重的巨石。他们编译、打包、上传，眼看着附件目录如山间碎石般杂乱无章。每一次整理都是徒劳，因为新的附件如潮水般涌来，将文件夹再度吞没。
+
+然而，我们应当设想，这位出题人是幸福的。
+
+幸福个头，我再也不要出有附件的题目了，而且根本没人做 PWN 题，人人都爱 Misc，如果放段 Base64 所有 Misc 手都会来做的吧。
+
+```
+f0VMRgEBAQAAAAAAAAAAAAIAAwABAAAATIAECCwAAAAAAAAAAAAAADQAIAABAAAAAAAAAACABAgAgAQIewAAAHsAAAAFAAAAABAAAIPsIGoEWDHbQ2hwgAQIWWoLWs2AagNYS4nhaiBazYCDxBj/5HNvIHRpbnkuLi4K
+```
+
+## 题目解析
+
+<analysis>
---
+++ challenges/pwn/tinyelf/build/src/tinyelf.asm
@@ -0,0 +1,58 @@
+BITS 32
+    org     0x08048000
+ehdr:
+        db      0x7F, "ELF", 1, 1, 1, 0         ;   e_ident
+times 8 db      0
+        dw      2                               ;   e_type
+        dw      3                               ;   e_machine
+        dd      1                               ;   e_version
+        dd      _start                          ;   e_entry
+        dd      phdr - $$                       ;   e_phoff
+        dd      0                               ;   e_shoff
+        dd      0                               ;   e_flags
+        dw      ehdrsize                        ;   e_ehsize
+        dw      phdrsize                        ;   e_phentsize
+phdr:
+        dd      1                               ; e_phnum       ; p_type
+                                                ; e_shentsize
+        dd      0                               ; e_shnum       ; p_offset
+                                                ; e_shstrndx
+ehdrsize        equ     $ - ehdr
+        dd      $$                              ;   p_vaddr
+        dd      $$                              ;   p_paddr
+        dd      filesize                        ;   p_filesz
+        dd      filesize                        ;   p_memsz
+        dd      5                               ;   p_flags
+        dd      0x1000                          ;   p_align
+phdrsize        equ     $ - phdr
+
+_start:
+    sub esp, 32
+
+    ; Print welcome message
+    push 4          ; sys_write
+    pop eax
+    xor ebx, ebx
+    inc ebx         ; stdout (1)
+    push msg
+    pop ecx
+    push msglen
+    pop edx
+    int 0x80
+
+    ; Read user input (vulnerable)
+    push 3          ; sys_read
+    pop eax
+    dec ebx         ; stdin (0)
+    mov ecx, esp
+    push 32
+    pop edx
+    int 0x80
+
+    add esp, 24
+    jmp esp
+
+msg:    db "so tiny...", 10
+msglen  equ $ - msg
+
+filesize        equ     $ - $$
